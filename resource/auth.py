1f16e4a60d424109e4bc65e05991e9523f869bcc
Dell <dtnguyen358@gmail.com>
2021-08-13 17:43:35 +0700

    chat app

--- resource/auth.py
+++ resource/auth.py
@@ -1,6 +1,6 @@
-from app import app
 from flask import request, jsonify, make_response
 from flask_restful import Resource
+import app
 from database.model import Users
 from werkzeug.security import generate_password_hash, check_password_hash
 from database.db import db
@@ -30,8 +30,10 @@ class login_user(Resource):
         user = Users.query.filter_by(name=auth.username).first()
 
         if check_password_hash(user.password, auth.password):
+            app_config = app.app.config
             token = jwt.encode(
-                {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
-            return jsonify({'token': token.decode('UTF-8')})
+                {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app_config['SECRET_KEY'])
+            # return jsonify({'token': token.decode('utf-8')})
+            return jsonify({'token': token})
 
         return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
