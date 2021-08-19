7616f57ced9cf79948c47f2598b02b1788b55322
Dell <dtnguyen358@gmail.com>
2021-08-19 16:57:28 +0700

    update db

--- database/model.py
+++ database/model.py
@@ -2,17 +2,26 @@ import datetime
 
 from .db import db
 
+info = db.Table('info',
+        db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+        db.Column('friend_id', db.Integer, db.ForeignKey('friends.id'), primary_key=True)
+)
+
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     password = db.Column(db.String(50))
-    group_chat = db.relationship('Group_chat',lazy='dynamic')
+    friends = db.relationship('Friends', secondary='info', lazy="dynamic")
+
+class Friends(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message_id = db.relationship('Messages', lazy="dynamic")
+    name = db.Column(db.String(50))
 
 
-class Group_chat(db.Model):
-    chat_id = db.Column(db.Integer, primary_key=True)
-    add_by = db.Column(db.Integer, db.ForeignKey('users.id'))
-    name_chat = db.Column(db.String(50))
-    box_chat = db.Column(db.String(255))
+class Messages(db.Model):
+    friend_id = db.Column(db.ForeignKey('friends.id'))
+    messages_id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.datetime.now())
-
--- resource/chat.py
+++ resource/chat.py
@@ -1,72 +1,72 @@
-from flask import request, Response, jsonify
-from flask_restful import Resource
-from database.model import Users, Group_chat
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from database.db import db
-import datetime
-
-class create_group_chat(Resource):
-    def get(self):
-        group_chat = Group_chat.query.all()
-        output = []
-        for group_data in group_chat:
-            data = {}
-            data['chat_id']= group_data.chat_id
-            data['add_by']= group_data.add_by
-            data['name_chat']= group_data.name_chat
-            data['box_chat']= group_data.box_chat
-            data['created_at']= group_data.created_at
-            output.append(data)
-        return jsonify({"list of message": output})
-
-    @jwt_required()
-    def post(self):
-        user_id = get_jwt_identity()
-        data = request.get_json(force=True)
-        group_chat = Group_chat(**data, add_by=user_id)
-        db.create_all()
-        db.session.add(group_chat)
-        db.session.commit()
-        return jsonify({"message": "successful to created a message"})
-
-class chat(Resource):
-    @jwt_required()
-    def put(self,chat_id):
-        data = request.get_json()
-        group_chat = Group_chat.query.get(chat_id)
-        if not group_chat:
-            return jsonify({"message": "doesnt exist group chat"})
-        if data.get('name_chat'):
-            group_chat.name_chat = data['name_chat']
-        if data.get('box_chat'):
-            group_chat.box_chat = data['box_chat']
-        db.session.add(group_chat)
-        db.session.commit()
-        return jsonify({"message": "update a message"})
-
-    @jwt_required()
-    def delete(self, chat_id):
-        group_chat = Group_chat.query.get(chat_id)
-        if not group_chat:
-            return jsonify({"message": "doesnt exist group chat"})
-        db.session.delete(group_chat)
-        db.session.commit()
-        return jsonify({"message": "chat deleted"})
-
-    @jwt_required()
-    def get(self, chat_id):
-        group_chat = Group_chat.query.filter_by(chat_id=chat_id)
-        output = []
-        for group_data in group_chat:
-            data = {}
-            data['chat_id'] = group_data.chat_id
-            data['add_by'] = group_data.add_by
-            data['name_chat'] = group_data.name_chat
-            data['box_chat'] = group_data.box_chat
-            data['created_at'] = group_data.created_at
-            output.append(data)
-        return jsonify({"chat id": output})
-
-
-
-
+# from flask import request, Response, jsonify
+# from flask_restful import Resource
+# from database.model import Users, Group_chat
+# from flask_jwt_extended import jwt_required, get_jwt_identity
+# from database.db import db
+# import datetime
+#
+# class create_group_chat(Resource):
+#     def get(self):
+#         group_chat = Group_chat.query.all()
+#         output = []
+#         for group_data in group_chat:
+#             data = {}
+#             data['chat_id']= group_data.chat_id
+#             data['add_by']= group_data.add_by
+#             data['name_chat']= group_data.name_chat
+#             data['box_chat']= group_data.box_chat
+#             data['created_at']= group_data.created_at
+#             output.append(data)
+#         return jsonify({"list of message": output})
+#
+#     @jwt_required()
+#     def post(self):
+#         user_id = get_jwt_identity()
+#         data = request.get_json(force=True)
+#         group_chat = Group_chat(**data, add_by=user_id)
+#         db.create_all()
+#         db.session.add(group_chat)
+#         db.session.commit()
+#         return jsonify({"message": "successful to created a message"})
+#
+# class chat(Resource):
+#     @jwt_required()
+#     def put(self,chat_id):
+#         data = request.get_json()
+#         group_chat = Group_chat.query.get(chat_id)
+#         if not group_chat:
+#             return jsonify({"message": "doesnt exist group chat"})
+#         if data.get('name_chat'):
+#             group_chat.name_chat = data['name_chat']
+#         if data.get('box_chat'):
+#             group_chat.box_chat = data['box_chat']
+#         db.session.add(group_chat)
+#         db.session.commit()
+#         return jsonify({"message": "update a message"})
+#
+#     @jwt_required()
+#     def delete(self, chat_id):
+#         group_chat = Group_chat.query.get(chat_id)
+#         if not group_chat:
+#             return jsonify({"message": "doesnt exist group chat"})
+#         db.session.delete(group_chat)
+#         db.session.commit()
+#         return jsonify({"message": "chat deleted"})
+#
+#     @jwt_required()
+#     def get(self, chat_id):
+#         group_chat = Group_chat.query.filter_by(chat_id=chat_id)
+#         output = []
+#         for group_data in group_chat:
+#             data = {}
+#             data['chat_id'] = group_data.chat_id
+#             data['add_by'] = group_data.add_by
+#             data['name_chat'] = group_data.name_chat
+#             data['box_chat'] = group_data.box_chat
+#             data['created_at'] = group_data.created_at
+#             output.append(data)
+#         return jsonify({"chat id": output})
+#
+#
+#
+#
